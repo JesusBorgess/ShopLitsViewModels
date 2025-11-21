44bb2db208a31450c22515e9470c896823117687
Jesus Nazario Borges Camal <23470139@campeche.tecnm.mx>
2025-11-20 20:31:11 -0600

    version funcinal de la implementacion de MVVM para agregar elementos a la lista de compras

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -8,126 +8,78 @@ using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public  partial class ShopListViewModel : ObservableObject 
+    public partial class ShopListViewModel : ObservableObject
     {
+
         [ObservableProperty]
         private string _nombreDelArticulo = string.Empty;
+
         [ObservableProperty]
         private int _cantidad = 1;
 
-        //private string _nombreDelArticulo = string.Empty;
-        //private int _cantidad = 1;
 
-        //public event NotifyCollectionChangedEventHandler? PropertyChanged;
 
-        //public string NombreDelArticulo
-        //{
-        //    get => _nombreDelArticulo;
-        //        set
-        //    {
-        //        if (_nombreDelArticulo != value)
-        //        {
-        //            _nombreDelArticulo = value;
-        //            OnProPertyChanged(nameof(NombreDelArticulo));
-        //        }
-        //    }
-        //}
-        //public int Cantidad
-        //{
-        //    get => _cantidad;
-        //    set
-        //    {
-        //        if ( _cantidad != value)
-        //        {
-        //            _cantidad = value;
-        //            OnProPertyChanged(nameof(Cantidad));
-        //        }
-        //    }
-        //}
-        public ObservableCollection<ShopListItem> ShopList { get;}
-        public ICommand AddShopListItemCommand
-        {
-            get;
-            private set;
-        }
+        public ObservableCollection<ShopListItem> ShopList { get; }
+
+
         public ShopListViewModel()
         {
             ShopList = new ObservableCollection<ShopListItem>();
             CargarDatos();
-            //AddShopListItemCommand = new Command(AddShopListItem);
         }
+
+
+
         [RelayCommand]
-        public void AddShopListItem;
-
-        
-        event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
-        {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
-        }
-
         public void AddShopListItem()
         {
-            if (string.IsNullOrEmpty(NombreDelArticulo) || _cantidad <=0)
-               
-            {
+            if (string.IsNullOrWhiteSpace(NombreDelArticulo) || Cantidad <= 0)
                 return;
-            }
-            Random generador = new Random();
-            var item = new ShopListItem
+
+            var generador = new Random();
+
+            var item = new ShopListItem()
             {
                 Id = generador.Next(),
                 Nombre = NombreDelArticulo,
-                Cantidad = this.Cantidad,
-                Comprado = false,
+                Cantidad = Cantidad,
+                Comprado = false
             };
+
             ShopList.Add(item);
+
             NombreDelArticulo = string.Empty;
             Cantidad = 1;
-                
         }
+
+
 
         private void CargarDatos()
         {
-
             ShopList.Add(new ShopListItem()
             {
                 Id = 1,
                 Nombre = "Pan de caja",
                 Cantidad = 1,
-                Comprado = false,
+                Comprado = false
             });
+
             ShopList.Add(new ShopListItem()
             {
-                Id = 1,
+                Id = 2,
                 Nombre = "Leche",
                 Cantidad = 3,
-                Comprado = false,
+                Comprado = false
             });
+
             ShopList.Add(new ShopListItem()
             {
-                Id = 1,
+                Id = 3,
                 Nombre = "Queso",
                 Cantidad = 100,
-                Comprado = false,
+                Comprado = false
             });
         }
-
-        //private void OnProPertyChanged (string propertyName)
-        //{
-        //    PropertyChanged?.Invoke(
-        //        this,
-        //        new PropertyChangingEventArgs(propertyName));
-        //}
-
-    
-
     }
+
 }

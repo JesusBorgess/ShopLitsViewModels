5d54a94e14a55fa30b6881342e2663f66c5f80f7
Jesus Nazario Borges Camal <23470139@campeche.tecnm.mx>
2025-12-09 14:01:13 -0600

    Primeros pasos para la Base de datos

---
+++ ShopList.Gui/Persistence/Configuration/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopList.Gui.Persistence.Configuration
+{
+    public static class Constants
+    {
+        public const string DatabaseFileName =
+            "ShopListDB.db3";
+        public static string DatabasePath
+            => Path.Combine(FileSystem.AppDataDirectory,
+                DatabaseFileName);
+        public const SQLite.SQLiteOpenFlags flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+    }
+}
